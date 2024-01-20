a3915005fd333bcda05cd1935607dc5d7ee0076a
Per Olsen <persteen22@gmail.com>
2024-01-20 14:03:56 +0100

    Tested workflow after domain renamed

--- api/index.py
+++ api/index.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World - from Python and Flask !'
+    return 'Hello World - from Python and Flask hosted at vercel'
 
 @app.route('/about')
 def about():
-    return 'Last updated: 07-01-2024 - A Python and Flask Web App'
+    return 'Last updated: 21-01-2024 - A Python and Flask Web App'
