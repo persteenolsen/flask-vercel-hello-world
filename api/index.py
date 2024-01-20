b85f534df70083b9c3da77c0bcd28003b81a26fc
Per Olsen <persteen22@gmail.com>
2024-01-20 14:05:44 +0100

    Took care of a spelling error

--- api/index.py
+++ api/index.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World - from Python and Flask hosted at vercel'
+    return 'Hello World - from Python and Flask hosted at Vercel'
 
 @app.route('/about')
 def about():
-    return 'Last updated: 21-01-2024 - A Python and Flask Web App'
+    return 'Last updated: 20-01-2024 - A Python and Flask Web App'
